b18b373919b8f600236a1e2b381ff26095abb653
Nisasaje24i <sulisnisa35@gmail.com>
2023-04-09 06:51:11 +0700

    merubah isi dari folder praktikum

--- 24_Introduction Rest Api/Praktikum/rest_api2/lib/main.dart
+++ 24_Introduction Rest Api/Praktikum/rest_api2/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rest_api2/ekplorasi.dart';
-import 'package:rest_api2/prioritas%201/post_contact.dart';
+import 'package:rest_api2/prioritas%201/post_get_contact.dart';
+import 'package:rest_api2/prioritas%201/put_data.dart';
 import 'package:rest_api2/prioritas2.dart';
 
 void main() {
@@ -14,13 +15,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'DiceBear Avatar',
+      title: 'REST API',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: PrioritasDua(),
-      home: Eksplorasi(),
-      // home: TambahKontak(),
+      home: PrioritasDua(),
+      // home: Eksplorasi(),
+      // home: PostGet(), //prioritas 1 no 1 dan 2
+      // home: const PutData(), //prioritas 1 no 3
     );
   }
 }
